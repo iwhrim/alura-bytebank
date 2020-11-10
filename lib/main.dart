0ccdb7da6c7d1a8788dff6f28bf9a2c5ebd55121
Gustavo Teixeira <iwhrim@gmail.com>
2020-11-09 23:38:14 -0300

    Implementado a busca e visualizacao dos contatos do banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:first_project/database/app_database.dart';
-import 'package:first_project/models/Contato.dart';
 import 'package:first_project/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(Bytebank());
-    findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
 class Bytebank extends StatelessWidget {
--- lib/screens/contatos/lista.dart
+++ lib/screens/contatos/lista.dart
@@ -1,4 +1,7 @@
+import 'package:first_project/database/app_database.dart';
+import 'package:first_project/models/Contato.dart';
 import 'package:first_project/screens/contatos/formulario.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContatosLista extends StatelessWidget {
@@ -8,21 +11,31 @@ class ContatosLista extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contatos'),
       ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alex',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
+      body: FutureBuilder<List<Contato>>(
+        initialData: List(),
+        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            final List<Contato> contacts = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final Contato contact = contacts[index];
+                return _ContatoItem(contact);
+              },
+              itemCount: contacts.length,
+            );
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                Text('Carregando...')
+              ],
             ),
-          )
-        ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -37,6 +50,29 @@ class ContatosLista extends StatelessWidget {
               );
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContatoItem extends StatelessWidget {
+  final Contato contato;
+
+  _ContatoItem(this.contato);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(
+          contato.nome,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contato.numeroConta.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
