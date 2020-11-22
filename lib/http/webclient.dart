9217762b841ff04a652afabad13eb3d97fc3ee89
Gustavo Teixeira <iwhrim@gmail.com>
2020-11-22 16:22:27 -0300

    Implementado comportamento de salvar transferencia

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,9 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:first_project/models/Contato.dart';
 import 'package:first_project/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
+
+final Client client =
+    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+
+const String baseURL = 'http://192.168.100.220:8080/transactions';
 
 class LoggingInterceptor implements InterceptorContract {
   @override
@@ -26,12 +32,9 @@ class LoggingInterceptor implements InterceptorContract {
 }
 
 Future<List<Transaction>> findAll() async {
-  final Client client =
-      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
 
-  final Response response = await client
-      .get('http://192.168.100.220:8080/transactions')
-      .timeout(Duration(seconds: 5));
+  final Response response =
+      await client.get(baseURL).timeout(Duration(seconds: 5));
   final List<dynamic> decodeJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
 
@@ -48,4 +51,32 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.nome,
+      'accountNumber': transaction.contact.numeroConta
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseURL, headers: {
+    'Content-type' : 'application/json',
+    'password' : '1000'
+  }, body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contato(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:first_project/http/webclient.dart';
+import 'package:first_project/models/Contato.dart';
+import 'package:first_project/models/transaction.dart';
 import 'package:first_project/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(Bytebank());
+  save(Transaction(200.0, Contato(0, 'Priston Tale', 2000)))
+      .then((value) => print(value));
   findAll().then((value) => print(value));
 }
 
