5ab45ed7327ee0f16d4c4057d98aefb27025fde3
Gustavo Teixeira <iwhrim@gmail.com>
2020-11-26 20:21:57 -0300

    Exemplo de contador utilizando Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:first_project/screens/dashboard.dart';
+import 'package:first_project/screens/counter.dart';
 import 'package:flutter/material.dart';
 import 'components/bytebank_theme.dart';
 
@@ -11,7 +11,7 @@ class Bytebank extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: bytebankTheme,
-      home: Dashboard(),
+      home: CounterPage(),
     );
   }
 }
---
+++ lib/screens/counter.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+// Exemplo de contador utilizando Bloc
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+
+  void decrement() => emit(state - 1);
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: CounterView(),
+    );
+  }
+}
+
+class CounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contador'),
+      ),
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, count) => Center(child: Text('$count')),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().increment(),
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().decrement(),
+            child: const Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
+  }
+}
